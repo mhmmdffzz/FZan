ad5d25cb7fd58548618220f93f8572d6931f5017
Muhammad Fauzan <fauzan072.mf@gmail.com>
2025-10-02 15:16:52 +0700

    Pengenalan Bahasa C++ (Bagian Kedua)

--- Pertemuan2_Modul2/UNGUIDED/Unguided1.cpp
+++ Pertemuan2_Modul2/UNGUIDED/Unguided1.cpp
@@ -1,6 +1,50 @@
 
 #include <iostream>
 using namespace std;
+int main() {
+    int arrA[3][3] = {
+        {7, 12, 22},
+        {31, 6, 41},
+        {15, 19, 36}
+    };
+    int arrB[3][3] = {
+        {11, 34, 7},
+        {3, 25, 41},
+        {5, 18, 33}
+    };
+    int arrC[3][3] = {0};
+    int pilihan;
+
+    do {
+        cout << "--- Menu Program Matriks ---" << endl;
+        cout << "1. Penjumlahan matriks" << endl;
+        cout << "2. Pengurangan matriks" << endl;
+        cout << "3. Perkalian matriks" << endl;
+        cout << "4. Keluar" << endl;
+        cout << "Masukkan pilihan Anda (1-4): ";
+        cin >> pilihan;
+
+        switch (pilihan) {
+            case 1:
+                jumlahMatriks(arrA, arrB, arrC);
+                break;
+            case 2:
+                kurangMatriks(arrA, arrB, arrC);
+                break;
+            case 3:
+                kaliMatriks(arrA, arrB, arrC);
+                break;
+            case 4:
+                cout << "Keluar dari program." << endl;
+                break;
+            default:
+                cout << "Pilihan tidak valid. Silakan coba lagi." << endl;
+        }
+        cout << endl; // Tambahkan baris kosong untuk pemisah antar menu
+    } while (pilihan != 4);
+
+    return 0;
+}
 
 void jumlahMatriks(int arrA[3][3], int arrB[3][3], int arrC[3][3]){
     for (int i = 0; i < 3; i++){
@@ -46,47 +90,6 @@ void kaliMatriks(int arrA[3][3], int arrB[3][3], int arrC[3][3]){
         cout << endl;
     }
 }
-int main() {
-    int arrA[3][3] = {
-        {7, 12, 22},
-        {31, 6, 41},
-        {15, 19, 36}
-    };
-    int arrB[3][3] = {
-        {11, 34, 7},
-        {3, 25, 41},
-        {5, 18, 33}
-    };
-    int arrC[3][3] = {0};
-    int pilihan;
-
-    do {
-        cout << "--- Menu Program Matriks ---" << endl;
-        cout << "1. Penjumlahan matriks" << endl;
-        cout << "2. Pengurangan matriks" << endl;
-        cout << "3. Perkalian matriks" << endl;
-        cout << "4. Keluar" << endl;
-        cout << "Masukkan pilihan Anda (1-4): ";
-        cin >> pilihan;
-
-        switch (pilihan) {
-            case 1:
-                jumlahMatriks(arrA, arrB, arrC);
-                break;
-            case 2:
-                kurangMatriks(arrA, arrB, arrC);
-                break;
-            case 3:
-                kaliMatriks(arrA, arrB, arrC);
-                break;
-            case 4:
-                cout << "Keluar dari program." << endl;
-                break;
-            default:
-                cout << "Pilihan tidak valid. Silakan coba lagi." << endl;
-        }
-        cout << endl; // Tambahkan baris kosong untuk pemisah antar menu
-    } while (pilihan != 4);
 
     return 0;
 }
